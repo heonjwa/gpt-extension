94753e81cf7b8c9b9dde80572e1af7422b5c1bc0
heonjwa <heonjwa@gmail.com>
2025-04-28 18:40:59 -0700

    extension pops up when chatgpt opens

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -17,8 +17,18 @@ export default defineBackground({
           function (response) {
             console.info("response", response);
           }
-          
         );
+      }
+    });
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      // Make sure the page is completely loaded
+      if (changeInfo.status === 'complete' && tab.url) {
+        // Check if the URL contains your target website
+        if (tab.url.includes('chatgpt.com')) {
+          // Open your popup programmatically
+          chrome.action.openPopup();
+        }
       }
     });
   }
